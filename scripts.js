846a0eb76596e2ad76b61987464080af438dd526
Marcel Schoombee <marcelsch01@yahoo.com>
2024-05-10 08:57:14 +0200

    Validation when values are missing

--- scripts.js
+++ scripts.js
@@ -6,5 +6,8 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = Math.floor (dividend / divider); // Math.floor rounds the number of.
-  
+    if (form.elements.namedItem('dividend', 'divider').value === "") {
+      alert("Division not performed. Both values are required in inputs. Try again");
+      valid = false;  // Validation when values are missing
+    }
 });
