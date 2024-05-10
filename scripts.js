543c3427cb51281903d127fe4eab9ca3a5992db3
Marcel Schoombee <marcelsch01@yahoo.com>
2024-05-10 09:56:30 +0200

    Anything not a number shows an error and fixed missing value warning

--- scripts.js
+++ scripts.js
@@ -5,18 +5,28 @@ form.addEventListener("submit", (event) => {
   event.preventDefault();
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
+  
   result.innerText = Math.floor (dividend / divider); // Math.floor rounds the number of.
+  
   const dividendNum = parseFloat(dividend);
   const dividerNum = parseFloat(divider);  
-    if (isNaN(dividendNum) || isNaN(dividerNum)) {
-      alert("Division not performed. Both values are required in inputs. Try again");
-      valid = false;  // Validation when values are missing
-    }
-    if (dividerNum === 0) {
-      alert("Division not performed. Invalid number provided. Try again")
-      console.error("Division not performed. Invalid number provided. Try again")
-      return dividend / divider;  // Invalid division logs error in console
-    }
     
+      if (dividend.trim() === "" || divider.trim() === "") {
+        alert("Division not performed. Both values are required in inputs. Try again");
+        console.error("Division not performed. Both values are required in inputs. Try again");
+        return;  // Validation when values are missing
+      }
+
+      if (dividerNum === 0) {
+        alert("Division not performed. Invalid number provided. Try again");
+        console.Error("Division not performed. Invalid number provided. Try again");
+        return dividend / divider; // Division not performed. Invalid number provided. Try again
+      }
+    
+      if (isNaN(dividendNum) || isNaN(dividerNum)) {
+        alert("Something critical went wrong. Please reload the page.");
+        console.error("Something critical went wrong. Please reload the page.");
+        return; // Anything not a number shows an error
+      }
 });
 
