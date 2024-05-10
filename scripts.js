6364e67bf7941bfd6cc66aac896d25c1f1a75b7d
Marcel Schoombee <marcelsch01@yahoo.com>
2024-05-10 09:30:25 +0200

    Invalid division logs error in console

--- scripts.js
+++ scripts.js
@@ -6,8 +6,17 @@ form.addEventListener("submit", (event) => {
   const entries = new FormData(event.target);
   const { dividend, divider } = Object.fromEntries(entries);
   result.innerText = Math.floor (dividend / divider); // Math.floor rounds the number of.
-    if (form.elements.namedItem('dividend', 'divider').value === "") {
+  const dividendNum = parseFloat(dividend);
+  const dividerNum = parseFloat(divider);  
+    if (isNaN(dividendNum) || isNaN(dividerNum)) {
       alert("Division not performed. Both values are required in inputs. Try again");
       valid = false;  // Validation when values are missing
     }
+    if (dividerNum === 0) {
+      alert("Division not performed. Invalid number provided. Try again")
+      console.error("Division not performed. Invalid number provided. Try again")
+      return dividend / divider;  // Invalid division logs error in console
+    }
+    
 });
+
